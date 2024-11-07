994a586b9cdfb7b7071219a5ba10da4e451e77c4
matheusgondra <matheusgondra951@gmail.com>
2024-11-07 15:10:40 -0300

    feat: ensure SignUpController return a message on success

--- src/application/controllers/sign-up.controller.ts
+++ src/application/controllers/sign-up.controller.ts
@@ -1,18 +1,20 @@
 import { AddAccount } from "@/domain/use-cases/add-account";
 import { ConflictException, Inject } from "@nestjs/common";
-import { SignUpRequestDTO } from "../dto/sign-up-request.dto";
 import { HttpController } from "../protocols/controller";
+import { SignUpRequestDTO, SignUpResponseDTO } from "../dto";
 
-export class SignUpController implements HttpController<SignUpRequestDTO, void> {
+export class SignUpController implements HttpController<SignUpRequestDTO, SignUpResponseDTO> {
 	constructor(
 		@Inject("AddAccount")
 		private readonly addAccount: AddAccount
 	) {}
 
-	async handle(request: SignUpRequestDTO): Promise<void> {
+	async handle(request: SignUpRequestDTO): Promise<SignUpResponseDTO> {
 		const user = await this.addAccount.add(request);
 		if (!user) {
 			throw new ConflictException("User already exists");
 		}
+
+		return new SignUpResponseDTO("Account created successfully");
 	}
 }
