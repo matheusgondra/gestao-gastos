2609fdb31dabfafdce7cb4ee2aa4c5c73550388e
matheusgondra <matheusgondra951@gmail.com>
2024-11-11 07:33:18 -0300

    feat: ensure AddAccountService call HashGenerator with correct password

--- src/data/services/add-account.service.ts
+++ src/data/services/add-account.service.ts
@@ -1,15 +1,19 @@
 import { AddAccount, AddAccountParams, AddAccountResult } from "@/domain/use-cases/add-account";
 import { Inject, Injectable } from "@nestjs/common";
-import { AddAccountRepository } from "../protocols";
+import { AddAccountRepository, HashGenerator } from "../protocols";
 
 @Injectable()
 export class AddAccountService implements AddAccount {
 	constructor(
 		@Inject("AddAccountRepository")
-		private readonly addAccountRepository: AddAccountRepository
+		private readonly addAccountRepository: AddAccountRepository,
+
+		@Inject("HashGenerator")
+		private readonly hashGenerator: HashGenerator
 	) {}
 
 	async add(accountData: AddAccountParams): Promise<AddAccountResult> {
+		await this.hashGenerator.hash(accountData.password);
 		await this.addAccountRepository.add(accountData);
 		return null;
 	}
