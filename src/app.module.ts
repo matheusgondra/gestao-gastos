661635cf9d00a54ac51ce6d06da0e040aaf2165e
Anderson Naz <anderson_1004@hotmail.com>
2024-11-25 04:22:16 -0300

    feat: update user app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { UserModule } from "./user/user.module";
 import { UserController } from "./user/user.controller";
 import { AdapterModule } from "./adapter/adapter.module";
 import { ConfigModule } from "@nestjs/config";
+import { UserService } from "./user/user.service";
 
 @Module({
 	imports: [ConfigModule.forRoot({ isGlobal: true, envFilePath: ".env" }), DatabaseModule, UserModule, AdapterModule],
 	controllers: [UserController],
-	providers: []
+	providers: [UserService]
 })
 export class AppModule {}
