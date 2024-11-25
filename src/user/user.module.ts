36e76f6c00cd1184c28406a32c9fe0b34ca689a3
Anderson Naz <anderson_1004@hotmail.com>
2024-11-25 04:22:58 -0300

    feat: removed unused service

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,13 +1,10 @@
 import { Module } from "@nestjs/common";
 import { UserService } from "./user.service";
-import { UserController } from "./user.controller";
 import { DatabaseModule } from "@/database/database.module";
 import { AdapterModule } from "@/adapter/adapter.module";
 
 @Module({
 	imports: [DatabaseModule, AdapterModule],
-	controllers: [UserController],
-	providers: [UserService],
-	exports: [UserService]
+	providers: [UserService]
 })
 export class UserModule {}
