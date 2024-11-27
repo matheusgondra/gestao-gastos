9d2e6a5cf3ef9f2925cc2803f3d9fe6538ff0c9e
Anderson Naz <anderson_1004@hotmail.com>
2024-11-27 13:10:51 -0300

    test: ensure return a message on success

--- test/domain/auth/controllers/sign-up.controller.spec.ts
+++ test/domain/auth/controllers/sign-up.controller.spec.ts
@@ -1,5 +1,5 @@
 import { SignUpController } from "@/domain/auth/controllers/signup.controller";
-import { SignUpRequestDTO } from "@/domain/auth/dto";
+import { SignUpRequestDTO, SignUpResponseDTO } from "@/domain/auth/dto";
 import { SignUpService } from "@/domain/auth/services/signup.service";
 import { ConflictException } from "@nestjs/common";
 import { Test, TestingModule } from "@nestjs/testing";
@@ -42,5 +42,10 @@ describe("SignUp Controller", () => {
 		jest.spyOn(signupServiceStub, "execute").mockResolvedValueOnce(null);
 		const promise = sut.handle(signUpRequestDTO);
 		await expect(promise).rejects.toThrow(new ConflictException("User already exists"));
+	});
+
+	it("should return a message on success", async () => {
+		const httpResponse = await sut.handle(signUpRequestDTO);
+		expect(httpResponse).toEqual(new SignUpResponseDTO("Account created successfully"));
 	});
 });
