16213d5c9cffa5fa42f1cc1e7a2ac1f86689b6be
Anderson Naz <anderson_1004@hotmail.com>
2024-11-27 19:13:11 -0300

    test: ensure call loadUserByEmail with correct param

---
+++ test/domain/auth/services/sign-in.service.spec.ts
@@ -0,0 +1,64 @@
+import { SignInService } from "@/domain/auth/services/signin.service";
+import { BCryptAdapter } from "@/infra/cryptography/bcrypt.adapter";
+import { UserRepository } from "@/infra/database/repositories/user.repository";
+import { JwtService } from "@nestjs/jwt";
+import { Test, TestingModule } from "@nestjs/testing";
+
+describe("SignInService", () => {
+	let sut: SignInService;
+	let databaseStub: UserRepository;
+	let hashServiceStub: BCryptAdapter;
+	let tokenServiceStub: JwtService;
+
+	const fakeUser = {
+		id: "any_id",
+		firstName: "any_first_name",
+		lastName: "any_last_name",
+		email: "any_email",
+		password: "any_password",
+		created_at: new Date(),
+		updated_at: new Date()
+	};
+
+	const signInData = {
+		email: "any_email",
+		password: "hashed_password"
+	};
+
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [
+				SignInService,
+				{
+					provide: UserRepository,
+					useValue: {
+						loadUserByEmail: jest.fn().mockResolvedValue(fakeUser)
+					}
+				},
+				{
+					provide: BCryptAdapter,
+					useValue: {
+						hashCompare: jest.fn().mockResolvedValueOnce(true)
+					}
+				},
+				{
+					provide: JwtService,
+					useValue: {
+						signAsync: jest.fn().mockResolvedValueOnce("any_access_token")
+					}
+				}
+			]
+		}).compile();
+
+		sut = module.get(SignInService);
+		databaseStub = module.get(UserRepository);
+		hashServiceStub = module.get(BCryptAdapter);
+		tokenServiceStub = module.get(JwtService);
+	});
+
+	it("should call loadUserByEmail with correct param", async () => {
+		const loadSpy = jest.spyOn(databaseStub, "loadUserByEmail");
+		await sut.execute(signInData);
+		expect(loadSpy).toHaveBeenCalledWith(signInData.email);
+	});
+});
