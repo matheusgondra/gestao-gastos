1fb2a9efa1796a4d84788fe1b6986128140967cb
Anderson Naz <anderson_1004@hotmail.com>
2024-11-27 16:40:14 -0300

    feat: update auth module

--- src/domain/auth/auth.module.ts
+++ src/domain/auth/auth.module.ts
@@ -3,10 +3,22 @@ import { DatabaseModule } from "@/infra/database/database.module";
 import { Module } from "@nestjs/common";
 import { SignUpController } from "./controllers/signup.controller";
 import { SignUpService } from "./services/signup.service";
+import { SignInService } from "./services/signin.service";
+import { JwtModule } from "@nestjs/jwt";
+import { jwtConstants } from "./constants/constants";
+import { SignInController } from "./controllers/signin.controller";
 
 @Module({
-	imports: [DatabaseModule, CryptographyModule],
-	controllers: [SignUpController],
-	providers: [SignUpService]
+	imports: [
+		JwtModule.register({
+			global: true,
+			secret: jwtConstants.secret,
+			signOptions: { expiresIn: "6000s" }
+		}),
+		DatabaseModule,
+		CryptographyModule
+	],
+	controllers: [SignUpController, SignInController],
+	providers: [SignUpService, SignInService]
 })
 export class AuthModule {}
