c7e8041554fc3e0be8718490f94643c3cfa18a1b
Anderson Naz <anderson_1004@hotmail.com>
2024-11-28 09:18:42 -0300

    test: signInController e2e return an accessToken on success

---
+++ test/domain/auth/controllers/sign-in.controller.e2e.spec.ts
@@ -0,0 +1,66 @@
+import { AuthModule } from "@/domain/auth/auth.module";
+import { SignInController } from "@/domain/auth/controllers/signin.controller";
+import { SignInService } from "@/domain/auth/services/signin.service";
+import { CryptographyModule } from "@/infra/cryptography/cryptography.module";
+import { DatabaseModule } from "@/infra/database/database.module";
+import { PrismaService } from "@/infra/database/prisma/prisma.service";
+import { INestApplication, ValidationPipe } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { Test } from "@nestjs/testing";
+import * as request from "supertest";
+
+describe("SignIn Controller E2E", () => {
+	let app: INestApplication;
+	let prismaService: PrismaService;
+
+	beforeAll(async () => {
+		const module = await Test.createTestingModule({
+			controllers: [SignInController],
+			imports: [
+				ConfigModule.forRoot({ isGlobal: true, envFilePath: ".env" }),
+				DatabaseModule,
+				AuthModule,
+				CryptographyModule
+			],
+			providers: [SignInService]
+		}).compile();
+
+		prismaService = module.get<PrismaService>(PrismaService);
+		app = module.createNestApplication();
+		app.useGlobalPipes(
+			new ValidationPipe({
+				whitelist: true,
+				transform: true
+			})
+		);
+		await app.init();
+	});
+
+	beforeEach(async () => {
+		await prismaService.user.deleteMany();
+	});
+
+	afterAll(async () => {
+		await app.close();
+	});
+
+	it("should return an access token on success", async () => {
+		await request(app.getHttpServer())
+			.post("/signup")
+			.send({
+				firstName: "John",
+				lastName: "Doe",
+				email: "johndoe@mail.com",
+				password: "StrongP@ssword123"
+			})
+			.expect(201)
+			.expect({ message: "Account created successfully" });
+
+		const httpResponse = await request(app.getHttpServer()).post("/signin").send({
+			email: "johndoe@mail.com",
+			password: "StrongP@ssword123"
+		});
+		expect(201);
+		expect({ httpResponse });
+	});
+});
