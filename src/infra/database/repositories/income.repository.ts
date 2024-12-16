cf8dade664b1dd5ee1bd3f787a10351e4b419e98
matheusgondra <matheusgondra951@gmail.com>
2024-12-16 10:09:12 -0300

    feat: ensure IncomeRepository call PrismaService.create with correct value

--- src/infra/database/repositories/income.repository.ts
+++ src/infra/database/repositories/income.repository.ts
@@ -1,10 +1,21 @@
 import { Income } from "@/domain/income/entities/income.entity";
 import { Injectable } from "@nestjs/common";
 import { IncomeRepositoryEntity } from "../entities/income.entity";
+import { PrismaService } from "../prisma/prisma.service";
 
 @Injectable()
 export class IncomeRepository {
-	async add(income: Income): Promise<IncomeRepositoryEntity> {
+	constructor(private readonly database: PrismaService) {}
+
+	async add(income: Income, userId: string): Promise<IncomeRepositoryEntity> {
+		await this.database.income.create({
+			data: {
+				value: income.value,
+				description: income.description,
+				date: income.date,
+				userId
+			}
+		});
 		return null;
 	}
 }
