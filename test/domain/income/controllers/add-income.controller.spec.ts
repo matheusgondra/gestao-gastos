8e95b2f002bc9a0200e2940c69e196e105add770
matheusgondra <matheusgondra951@gmail.com>
2024-12-16 07:51:24 -0300

    test: ensure AddIncomeController call AddIncomeService with correct value

---
+++ test/domain/income/controllers/add-income.controller.spec.ts
@@ -0,0 +1,46 @@
+import { AuthGuard } from "@/domain/auth/auth.guard";
+import { AddIncomeController } from "@/domain/income/controllers/add-income.controller";
+import { AddIncomeRequestDTO } from "@/domain/income/dto/add-income-request.dto";
+import { AddIncomeService } from "@/domain/income/services/add-income.service";
+import { Test } from "@nestjs/testing";
+
+describe("AddIncomeController", () => {
+	let sut: AddIncomeController;
+	let addIncomeService: AddIncomeService;
+
+	const requestDTO: AddIncomeRequestDTO = {
+		value: 100.0,
+		description: "any_description",
+		date: new Date("2021-01-01")
+	};
+
+	beforeEach(async () => {
+		const module = await Test.createTestingModule({
+			controllers: [AddIncomeController],
+			providers: [
+				{
+					provide: AddIncomeService,
+					useValue: {
+						execute: jest.fn()
+					}
+				}
+			]
+		})
+			.overrideGuard(AuthGuard)
+			.useValue({
+				canActivate() {
+					return true;
+				}
+			})
+			.compile();
+
+		sut = module.get(AddIncomeController);
+		addIncomeService = module.get(AddIncomeService);
+	});
+
+	it("should call AddIncomeService with correct value", async () => {
+		const executeSpy = jest.spyOn(addIncomeService, "execute");
+		await sut.handle(requestDTO);
+		expect(executeSpy).toHaveBeenCalledWith(requestDTO);
+	});
+});
