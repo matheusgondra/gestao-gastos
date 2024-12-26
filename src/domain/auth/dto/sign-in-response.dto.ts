c2873b16f886a7e9257946a521fb0e8cae4d5005
matheusgondra <matheusgondra951@gmail.com>
2024-12-26 07:34:27 -0300

    docs: add doc to SignInController

--- src/domain/auth/dto/sign-in-response.dto.ts
+++ src/domain/auth/dto/sign-in-response.dto.ts
@@ -1,4 +1,7 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class SignInResponseDTO {
+	@ApiProperty({ description: "JWT Token", example: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9" })
 	token: string;
 
 	constructor(token: string) {
